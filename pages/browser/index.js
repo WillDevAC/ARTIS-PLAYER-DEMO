f1320d917baefbc4533a9d1ba10a7dcb964faa1f
WillDevAC <wydemairomsilva@gmail.com>
2022-03-09 16:14:57 -0300

    start

---
+++ pages/browser/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { useUserAgent } from 'next-useragent'
+
+import { useEffect } from "react";
+import { useState } from "react";
+
+function Browser(props) {
+
+    const [ua, setUa] = useState()
+    
+    useEffect(()=>{
+      if (props.uaString) {
+        setUa(useUserAgent(props.uaString))
+      } else {
+        setUa(useUserAgent(window.navigator.userAgent))
+      }
+    })
+
+  return (
+    <>
+      {String(ua?.source).search("ArtisBrowser") > 0 ? <span>Página liberada, pois está sendo usado o browser artis</span>: <span>Página bloqueada, use o ArtisBrowser</span>}
+    </>
+  );
+};
+
+export default Browser;
